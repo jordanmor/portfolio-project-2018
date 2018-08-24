599e9337be539e4bb874aa38f71ffc2a48fedd4e
Jordan Mor <jordanmor@gmail.com>
2018-08-24 11:13:32 -0400

    Handle non-existent route errors

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ const mainRoutes = require('./routes');
 
 app.use(mainRoutes);
 
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use( (err, req, res, next) => {
+  res.locals.error = err;
+  console.log(`The following error has occurred: ${err.message} ${err.status}`);
+  res.status(err.status).render('error');
+});
+
 app.listen(3000, () => console.log('The application is running on localhost:3000!'));
