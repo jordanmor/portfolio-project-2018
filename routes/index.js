894547bfb71f6e4107dd880152e0f924a92f7c83
Jordan Mor <jordanmor@gmail.com>
2018-08-27 06:31:01 -0400

    Pass data as locals object to home and project pug templates

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { projects } = require('../data.json');
 
 router.get('/', (req, res) => {
+  res.locals.projects = projects;
   res.render('index');
 });
 
@@ -10,6 +12,7 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/projects/:id', (req, res) => {
+  res.locals.project = projects[req.params.id];
   res.render('project');
 });
 
