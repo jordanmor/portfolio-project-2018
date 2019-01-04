c487c64989d95bcb093311c7ab6c50246392301c
Jordan Mor <jordanmor@gmail.com>
2019-01-04 17:32:37 -0500

    Update portfolio bio

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 const app = express();
 
-/* The path provided to the express.static function is relative to the directory 
-from where the node process is loaded. If the express app is run from another directory, 
-it’s safer to use the absolute path of the directory that you want to serve */
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
@@ -13,10 +10,6 @@ app.set('view engine', 'pug');
 const mainRoutes = require('./routes');
 
 app.use(mainRoutes);
-
-// app.all('*', (req, res) => {
-//   res.redirect('/');
-// })
 
 // 404 errors
 app.use((req, res, next) => {
